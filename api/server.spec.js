28356d69b916f6bf32bf24c96c31f9d3c40b9829
Elizabeth Doyle <lizdoyle1121@gmail.com>
2019-12-13 01:09:30 -0500

    started testing

---
+++ api/server.spec.js
@@ -0,0 +1,27 @@
+const server = require('./server.js');
+const request = require('supertest');
+
+describe('server.js', () => {
+    test('should be the testing environment', () => {
+        expect(process.env.DB_ENV).toBe('testing');
+    });
+
+    describe('GET /', () => {
+        it('should return 200 OK', async () => {
+            const res = await request(server).get('/');
+            expect(res.status).toBe(200);
+        });
+
+        // does it return the right data type?
+        it('should be json', async () => {
+            const res = await request(server).get('/');
+            expect(res.type).toBe('application/json');
+        });
+
+        // does it return the right data?
+        it('should return the right object', async () => {
+            const res = await request(server).get('/');
+            expect(res.body).toEqual({ api: 'up' });
+        })
+    });
+});
